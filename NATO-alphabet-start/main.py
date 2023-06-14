2ff7c6f8d042cdd252e52dec10022ecfd273a43d
danie <greenhalghdan@gmail.com>
2023-06-14 08:27:44 +0100

    adding error handling

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,38 +1,23 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
 
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
 #TODO 1. Create a dictionary in this format:
-df = pandas.read_csv("nato_phonetic_alphabet.csv")
-# for (index, row) in df.iterrows():
-#     print(row.letter)
-dict = {row.letter:row.code for (index, row) in df.iterrows()}
-print(dict)
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
-sentance = input("whats the word you would like to spell out: ")
-list_sentance = list(sentance)
-# list = []
-#
-# for letter in list_sentance:
-list = [row.code for letter in list_sentance for (index, row) in df.iterrows() if row.letter == letter.upper() ]
-#list = [row.code for letter in list_sentance if row.letter == letter]
-print(f"Your spelling is: \n{list}")
+word = input("Enter a word: ").upper()
+error = True
+output_list = []
+while error:
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("You entered something that was not a letter!")
+        word = input("Enter a word: ").upper()
+    else:
+        error = False
+print(output_list)
